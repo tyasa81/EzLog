daeb5daac8eb8ba2a29b69b4e6c22fa37d496094
Tony Yasa <tyasa81@gmail.com>
2024-10-26 17:07:33 +0700

    change log function to take in model for loggable and acted by

--- src/EzLoggable.php
+++ src/EzLoggable.php
@@ -2,6 +2,8 @@
 
 namespace tyasa81\EzLoggable;
 
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Database\Eloquent\Model;
 use tyasa81\EzLoggable\Repositories\LogRepository;
 
 class EzLoggable
@@ -13,8 +15,8 @@ class EzLoggable
         $this->logRepository = new LogRepository($connection);
     }
 
-    public function log(int $user_id, string $loggable_type, int $loggable_id, string $acted_by_type, int $acted_by_id, string $action, string $column, string $before, string $after, ?string $description = null)
+    public function log(int $user_id, Model|Authenticatable $loggable, Model|Authenticatable $acted_by, string $action, string $column, string $before, string $after, ?string $description = null)
     {
-        return $this->logRepository->create($user_id, $loggable_type, $loggable_id, $acted_by_type, $acted_by_id, $action, $column, $before, $after, $description);
+        return $this->logRepository->create($user_id, $loggable->getMorphClass(), $loggable->id, $acted_by->getMorphClass(), $acted_by->id, $action, $column, $before, $after, $description);
     }
 }
