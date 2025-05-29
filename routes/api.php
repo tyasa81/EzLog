1d732aecb38def97557b8872dfb6dd702de3792b
Tony Yasa <tyasa81@gmail.com>
2025-05-29 18:01:05 +0700

    added eztoken gated middleware

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('api/v1')->namespace('tyasa81\EzLoggable\Http\Controllers\Api\v1')->group(function () {
-    Route::prefix('loggables')->middleware(['auth:sanctum', 'ability:dashboard', 'userlink.types:user'])->group(function () {
+    Route::prefix('loggables')->middleware(['auth:sanctum', 'ability:dashboard', 'userlink.types:user', 'eztokendeposit.gated'])->name('loggables')->group(function () {
         Route::get('/', 'LoggableController@list');
     });
 });
